329db3a09fd02b532feba65c7d304927cca96e0a
han-seung-pyo <36193411+han-seung-pyo@users.noreply.github.com>
2018-09-11 22:48:20 +0900

    Update Thomas algorism.py

--- Thomas algorism.py
+++ Thomas algorism.py
@@ -26,40 +26,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy as si
 
-#과제만을 위한...
-def thomas_al(alpha, size,d1,dn):
-    matrix =np.matrix(np.eye(size)*(1+alpha) + np.eye(size, k = -1) *(-alpha/2) + np.eye(size, k = 1) *(-alpha/2))
-    d= np.zeros(size)
-    d[0] , d[-1]= d1, dn
-    d = d*(alpha/2)
-    sol = np.linalg.solve(matrix, d).round(3)
-    return sol
+#%% 
+#inverse matrix를 이용하여 구하는 방법
+def thomas_al2(diagonal,l_diagonal,u_diagonal,d): return np.linalg.solve(np.matrix(np.diag(diagonal)+np.diag(l_diagonal,k=-1)+np.diag(u_diagonal,k=1)),d).round(3)
 
-
-#print('d1 = 0, dn = 1')
-#a = []
-#for i in np.arange(0.5,5.1,0.1):
-#    x = thomas_al(i,10,0,1).round(3)
-#    a.append(x)
-#sol_1 = pd.DataFrame(a, index = np.arange(0.5,5.1,0.1).round(1),columns = [ 'x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
-#
-#
-#
-#print('d1= 0, d1=5 ')
-#b = []
-#for i in np.arange(0.5,5.1,0.1):
-#    y = thomas_al(i,10,0,5).round(3)
-#    b.append(y)
-#sol_2 = pd.DataFrame(b, index = np.arange(0.5,5.1,0.1).round(1),columns = [ 'x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
-
-
-#project 진행할때는 이 방법이 나을 
-def thomas_al2(diagonal,l_diagonal,u_diagonal,d):
-    matrix = np.matrix(np.diag(diagonal)+np.diag(l_diagonal,k=-1)+np.diag(u_diagonal,k=1))
-    x = np.linalg.solve(matrix,d).round(3)
-    return x
-
-emp = []  #appending 위해서 만든 list
+emp = []
 for alpha in np.arange(0.5,5.1,0.1):
     a = np.ones(10) * (1+alpha) #diagonal
     b = np.ones(9) * (-alpha/2) #lower diagonal
@@ -70,8 +41,10 @@ for alpha in np.arange(0.5,5.1,0.1):
     d = d*alpha/2
     z = thomas_al2(a,b,c,d)
     emp.append(z)
-sol_3 = pd.DataFrame(emp, index = np.arange(0.5,5.1,0.1).round(1), columns = [ 'x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
+solution_inv = pd.DataFrame(emp, index = np.arange(0.5,5.1,0.1).round(1), columns = [ 'x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
+print ("using inverse matrix (d=1)")
 
+print ("="*30)
 
 emp = []
 for alpha in np.arange(0.5,5.1,0.1):
@@ -84,76 +57,55 @@ for alpha in np.arange(0.5,5.1,0.1):
     d = d*alpha/2
     z = thomas_al2(a,b,c,d)
     emp.append(z)    
-sol_4 = pd.DataFrame(emp, index = np.arange(0.5,5.1,0.1).round(1),columns = [ 'x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
+solution_inv2 = pd.DataFrame(emp, index = np.arange(0.5,5.1,0.1).round(1),columns = [ 'x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
 print ("using inverse matrix (d=5)")
-print (sol_4)
+print (solution_inv2)
 print ("="*30)
-def cal(a,b,c,d):
-    a1 =[a[0]]
-    d1 =[d[0]] 
-    for i in range(len(c)):
-        x = a[i+1] - (b[i+1] * c[i]/a1[i])
-        y = d[i+1] - (b[i+1] * d1[i]/a1[i])
-        a1.append(x)
-        d1.append(y)
-    return a1, d1
 
-##Thomas algorism에서 a prime 과 d prime이 이떻게 되는지 계산 해봄.
-emp_1 = []
-emp_2 = []
-emp_3 = []
+#%%
+#Thomas algorism을 통해 구한 방법
+alpha = 0.5
+diagonal = np.ones(10) * (1+alpha)
+l_diagonal = np.ones(10) * (-alpha/2)
+u_diagonal = np.ones(10) * (-alpha/2)
+d = np.array([0,0,0,0,0,0,0,0,0,1]) * (alpha/2)
+
+
+def cal(diagonal,l_diagonal,u_diagonal,d):
+    a1 =[diagonal[0]]
+    d1 =[d[0]]
+    x = []
+    for i in range(len(diagonal)-1):
+        a_prime = diagonal[i+1] - (l_diagonal[i+1] * u_diagonal[i]/a1[i])
+        d_prime = d[i+1] - (l_diagonal[i+1] * d1[i]/a1[i])
+        a1.append(a_prime)
+        d1.append(d_prime)
+    
+    x.append(d1[-1]/a1[-1])
+    
+    for i in range(9,0,-1):
+        x.append((-u_diagonal[i]/a1[i])*x[9-i])
+    x.reverse()
+    x = [x]
+    x= pd.DataFrame(x, columns = ['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10']).round(3)
+    return x
+x = cal(diagonal, l_diagonal, u_diagonal, d)
+
+
+solution_thomas = pd.DataFrame()
 for alpha in np.arange(0.5,5.1,0.1):
-    a= np.ones(10) * (1+alpha)
-    b = np.ones(10) * (-alpha/2)
-    b[0] = 0
-    c = np.ones(9) * (-alpha/2)
-    d = np.zeros(10)
-    d[0] =0
-    d[-1] = 1
-    d = d*alpha/2
-    a1, d1 = cal(a,b,c,d)
-    emp_1.append(a1)
-    emp_2.append(d1)
-    emp_3.append(c)
+    diagonal = np.ones(10) * (1+alpha)
+    l_diagonal = np.ones(10) * (-alpha/2)
+    u_diagonal = np.ones(10) * (-alpha/2)
+    d = np.array([0,0,0,0,0,0,0,0,0,1]) * (alpha/2)
+    solution_thomas = solution_thomas.append(cal(diagonal, l_diagonal, u_diagonal,d))
+solution_thomas.index = np.arange(0.5,5.1,0.1).round(2)
 
-# Thomas Algorithm d=1
-calculation_a = pd.DataFrame(emp_1, index = np.arange(0.5,5.1,0.1).round(1), columns= ['a1','a2','a3','a4','a5','a6','a7','a8','a9','a10'])
-calculation_d = pd.DataFrame(emp_2, index = np.arange(0.5,5.1,0.1).round(1), columns= ['d1','d2','d3','d4','d5','d6','d7','d8','d9','d10'])
-calculation_d5 = calculation_d * 5
-calculation_c = pd.DataFrame(emp_3, index = np.arange(0.5,5.1,0.1).round(1), columns= ['c1','c2','c3','c4','c5','c6','c7','c8','c9'])
-d_result = pd.DataFrame(index = np.arange(0.5,5.1,0.1).round(1), columns= ['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10'])
-
-d10 = calculation_d['d10']/calculation_a['a10']
-d_result['x10'] = d10
-
-for i in range(9,0,-1):
-    if i ==9:
-        d_result['x'+str(i)]=(calculation_d['d'+str(i)]/calculation_a['a'+str(i)])-(calculation_c['c'+str(i)]/calculation_a['a'+str(i)])*d10
-    else:
-        d_result['x'+str(i)]=(calculation_d['d'+str(i)]/calculation_a['a'+str(i)])-(calculation_c['c'+str(i)]/calculation_a['a'+str(i)])*d_result['x'+str(i+1)]
-
-print ("using thomas algorithm (d=1)")
-print (d_result.round(3))
-
-# Thomas Algorithm d=5
-d10 = calculation_d5['d10']/calculation_a['a10']
-d_result['x10'] = d10
-
-for i in range(9,0,-1):
-    if i ==9:
-        d_result['x'+str(i)]=(calculation_d5['d'+str(i)]/calculation_a['a'+str(i)])-(calculation_c['c'+str(i)]/calculation_a['a'+str(i)])*d10
-    else:
-        d_result['x'+str(i)]=(calculation_d5['d'+str(i)]/calculation_a['a'+str(i)])-(calculation_c['c'+str(i)]/calculation_a['a'+str(i)])*d_result['x'+str(i+1)]
-
-print ("using thomas algorithm (d=5)")
-print (d_result.round(3))
-
-
-
-
-# print ("="*30)
-# print (calculation_a)
-# print ("="*30)
-# print (calculation_d)
-# print ("="*30)
-# print (calculation_c)
+solution_thomas2 = pd.DataFrame()
+for alpha in np.arange(0.5,5.1,0.1):
+    diagonal = np.ones(10) * (1+alpha)
+    l_diagonal = np.ones(10) * (-alpha/2)
+    u_diagonal = np.ones(10) * (-alpha/2)
+    d = np.array([0,0,0,0,0,0,0,0,0,5]) * (alpha/2)
+    solution_thomas2 = solution_thomas2.append(cal(diagonal, l_diagonal, u_diagonal,d))
+solution_thomas2.index = np.arange(0.5,5.1,0.1).round(2)
